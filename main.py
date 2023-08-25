b72d40169a229f991d03693489d0cd8449f63943
Dandev0 <kokda@mail.ru>
2023-08-25 14:16:54 +0500

    updated main.py

--- main.py
+++ main.py
@@ -7,21 +7,25 @@ from arg import Args
 class Get_valute:
     def __init__(self):
         self.period = args.period
-        self.old_data_valute = None
+        self.old_exchange_rate = {
+                    'rub': None,
+                    'usd': None,
+                    'eur': None
+                }
 
     async def take_value_valutes(self, url):
         async with aiohttp.ClientSession() as request:
             async with request.get(url) as response:
                 response_valute = await response.json()
                 self.new_data_valute = {
-                    'RUB': response_valute['rates']['RUB'],
-                    'USD': response_valute['rates']['USD'],
-                    'EUR': response_valute['rates']['EUR']
+                    'rub': response_valute['rates']['RUB'],
+                    'usd': response_valute['rates']['USD'],
+                    'eur': response_valute['rates']['EUR']
                 }
                 global all_info_about_valute
-                all_info_about_valute = {'old_valute': self.old_data_valute, 'actual_valute': self.new_data_valute}
+                all_info_about_valute = {'old_valute': self.old_exchange_rate, 'actual_valute': self.new_data_valute}
                 for i in self.new_data_valute.keys():
-                    self.old_data_valute[i] = self.new_data_valute[i]
+                    self.old_exchange_rate[i] = self.new_data_valute[i]
 
     async def start_get_valute(self):
         url = f"https://openexchangerates.org/api/latest.json?app_id=3d3a6f24e1644a66a070bd8af9e6aaec&base=USD"
@@ -32,7 +36,6 @@ class Get_valute:
 
 class Balance(Get_valute):
     def __init__(self, post_data=None, need_valute=None):
-        super().__init__()
         self.old_balance = old_balance
         self.balance = balance
         self.base_valute = 'USD'
@@ -40,18 +43,22 @@ class Balance(Get_valute):
         self.need_valute = need_valute
 
     async def check_change_valute(self):
+        await asyncio.sleep(3)
+        print(self.balance, self.old_balance)
         if self.old_balance != self.balance:
             logger.warning(
                 f'Balance has been changed!\nOld balance: {self.old_balance}\nActual balance: {self.balance}')
-        if self.all_info_about_valute['actual_valute'] != self.all_info_about_valute['old_valute']:
+            self.old_balance = self.balance
+        if all_info_about_valute['actual_valute'] != all_info_about_valute['old_valute']:
             logger.warning(
-                f'Currency exchange rate has been changed!\nOld сurrency exchange rate: {self.old_data_valute}\nActual сurrency exchange rate: {self.new_data_valute}')
-        await asyncio.sleep(20)
+                f'Currency exchange rate has been changed!\nOld сurrency exchange rate: {all_info_about_valute["old_valute"]}\nActual сurrency exchange rate: {all_info_about_valute["actual_valute"]}')
+        await asyncio.sleep(2)
+        loop.create_task(self.check_change_valute())
 
     async def set_balance(self):
         for i in self.post_data.keys():
-            balance[i] = self.post_data[i]
-        return balance
+            self.balance[i] = self.post_data[i]
+        return self.balance
 
     async def get_balance(self):
         return self.balance
@@ -68,7 +75,8 @@ class Balance(Get_valute):
         self.rub = balance['rub']
         self.usd = balance['usd']
         self.eur = balance['eur']
-        self.sum_rub = self.rub + self.usd * self.new_data_valute['rub'] + self.eur * self.new_data_valute['usd'] * self.new_data_valute['rub']
+        self.data_balance = all_info_about_valute['actual_valute']
+        self.sum_rub = self.rub + self.usd * self.data_balance['rub'] + self.eur * self.data_balance['usd'] * self.data_balance['rub']
         self.sum_usd = self.usd + self.rub / self.usd + self.eur / self.usd
         resp = [self.sum_rub, self.sum_usd]
         return resp
@@ -76,8 +84,9 @@ class Balance(Get_valute):
 
 async def background_tasks(app):
     get_valute = Get_valute()
+    check_change = Balance()
     app['task'] = asyncio.create_task(get_valute.start_get_valute())
-    app['task1'] = asyncio.create_task(Balance().check_change_valute())
+    app['task1'] = asyncio.create_task(check_change.check_change_valute())
 
 
 def start_server():
@@ -121,9 +130,10 @@ class API:
 
 if __name__ == '__main__':
     api = API()
-    old_balance = None
     args = Args().get_args()
+    old_balance = {'rub': args.rub, 'usd': args.usd, 'eur': args.eur}
     balance = {'rub': args.rub, 'usd': args.usd, 'eur': args.eur}
+    old_exchange_rate = None
     period = args.period
     logger.debug('App is started')
     if args.debug.lower() in {'1', 'true', 'y'}:
@@ -137,7 +147,6 @@ if __name__ == '__main__':
                     web.get('/get/eur', api.get_eur),
                     web.get('/get/usd', api.get_usd),
                     web.get('/amount/get', api.get_amount),
-                    web.post('/modify', api.modify_balance),
-                    # (web.get('/', api.get_balance))
+                    web.post('/modify', api.modify_balance)
                     ])
     start_server()
